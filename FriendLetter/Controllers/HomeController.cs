ad756ce099e0d4511bc93d2a3cfa90cb6d2dff0d
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-12 19:51:04 -0700

    add LetterVariable.cs

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -5,8 +5,14 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
 
+    [Route("/goodbye")]
     public string GoodBye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public ActionResult Letter() { return View(); }
+    
   }
 }
